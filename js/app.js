e9a65458f3f5d687737d2c96e694a541a7431f16
maxravel <maxmaterniak@gmail.com>
2018-01-18 21:18:59 +0100

    sixth commit

--- js/app.js
+++ js/app.js
@@ -1,56 +1,64 @@
+// W O R K I N G    O N     D O C U M E N T
+
 // let val;
-// val =parseFloat(100.30);
+// val=document;
+// val=document.all[1];
+// val=document.all.length;
+// val=document.URL;
+// val=document.characterSet;
+// val=document.links[1];
+// val=document.images[1];
+// val=document.scripts[0].getAttribute("src");
+// val=document.contentType;
+// val=document.forms[0].method;
 // console.log(val);
-// console.log(typeof val);
-// console.log(val.toFixed(0));
 
-//DOM selectors
 
+
+//  C A L L B A C K forEach() 
+
+//let scripts = document.scripts;
+// console.log(scripts);
+// scripts.forEach(function(script){    - ERROR - scripts.forEach is not a function  (forEach is for arrays only)
+//     console.log(script);
+// });
+
+// let scriptsArr = Array.from(scripts);  // making array
+// scriptsArr.forEach(function(script){
+//     console.log(script.getAttribute("src"));
+// }); //   WORKING CODE
+
+
+
+// D O M   S E L E C T O R S
+
+// S I N G L E    E L E M E N T S     S E L E C T O R S 
+
+//  -  C H A N G E    S T Y L I N G
+
+//console.log(document.getElementById("parag1"));
 document.getElementById("parag1").style.background="yellow";
 document.getElementById("parag1").innerHTML='<span style="color:red">Lorem ipsum.</span>';
-
+//document.getElementById("parag1").style.display="none";
 const parag1 = document.getElementById("parag1");
 parag1.style.padding="20px";
-
 document.querySelector("#parag2").style.padding="20px";
 //console.log(document.querySelector("#parag2"));
-
 document.querySelector("h1").style.textAlign="center";
 document.querySelector("ul").style.padding="20px";
 document.querySelector(".box").style.padding="20px";
-
-let val;
-
-val=document;
-val=document.all[1];
-val=document.all.length;
-val=document.URL;
-val=document.characterSet;
-val=document.links[1];
-val=document.images[1];
-val=document.scripts[0].getAttribute("src");
-//console.log(val);
-
-//Callback forEach
-let scripts = document.scripts;
-let scriptsArr = Array.from(scripts);
-scriptsArr.forEach(function(script){
-    //console.log(script.getAttribute("src"));
-});
-
-
-// S I N G L E    E L E M E N T S     S E L E C T O R S 
-//change styling
 const start=document.getElementById("start");
 //console.log(start.className);
 start.style.background="white";
 //document.getElementById("start").style.display="none";
-//change content
+
+//  -  C H A N G E   C O N T E N T
+
 start.textContent="Chuck Norris";
 start.innerText="Andrzej Gołota";
 start.innerHTML='<h2 style="color:red">Tyson Fury<h2>';
 //document.querySelector() - much powerful, you can use everything to select, just like jQuery $()
-//console.log(document.querySelector('#start'));
+console.log(document.querySelector('#start'));
 document.querySelector('li').style.color="red"; // only first one is red, becouse it's single selector
 document.querySelector('li:last-child').style.color="green";
 document.querySelector('li:nth-child(3)').style.color="blue";
@@ -64,8 +72,8 @@ const aqua=document.getElementsByClassName('item');
 //console.log(collection[2]);
 aqua[5].style.background="aqua";
 
-const items=document.querySelectorAll(".item");
-items[4].style.background="aqua";
+//const items=document.querySelectorAll(".item");
+//items[4].style.background="aqua";
 
 const odd=document.querySelectorAll('li:nth-child(odd)');
 odd.forEach(function(li){
@@ -85,12 +93,36 @@ lis[3].style.color="aqua";
 lis[2].textContent="winner!";
 
 //convert to array
-lis=Array.from(lis);
-lis.reverse();
-console.log(lis);
+//lis=Array.from(lis);
+//lis.reverse();
+//console.log(lis);
 
-lis.forEach(function(li, index){
-    console.log(li.className);
+//lis.forEach(function(li, index){
+    //console.log(li.className);
     //li.textContent='${index} :hello'; //nie działa
-});
+//});
 
+
+
+// T R A V E R S I N G     D O M
+let vac;
+const list=document.querySelectorAll('ul.collection');
+const listItem=document.querySelectorAll('li.item:first-child');
+
+
+//get child node
+vac=list.childNodes;
+
+//get children element nodes
+vac=list.children;
+
+
+
+// R E P L A C I N G   E L E M E N T S
+//don't work
+// const newElement= document.createElement("h2");
+// newElement.id="newElem";
+// newElement.appendChild(document.createTextNode("New Element"));
+// const oldElement=document.getElementById("replacing-item");
+// const parent=document.querySelector("container");
+// parent.replace(newElement, oldElement);
